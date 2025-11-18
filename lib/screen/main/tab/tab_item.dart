8b5d863c65a3b24e29cc3fd0b8350e6a7f4a8527
logan <logan@nerdlab.xyz>
2025-11-18 21:22:48 +0900

    getx 변환 완료

--- lib/screen/main/tab/tab_item.dart
+++ lib/screen/main/tab/tab_item.dart
@@ -12,16 +12,19 @@ enum TabItem {
   final String tabName;
   final Widget firstPage;
 
-  const TabItem(this.activeIcon, this.tabName, this.firstPage, {IconData? inActiveIcon})
+  const TabItem(this.activeIcon, this.tabName, this.firstPage,
+      {IconData? inActiveIcon})
       : inActiveIcon = inActiveIcon ?? activeIcon;
 
-  BottomNavigationBarItem toNavigationBarItem(BuildContext context, {required bool isActivated}) {
+  BottomNavigationBarItem toNavigationBarItem(BuildContext context,
+      {required bool isActivated}) {
     return BottomNavigationBarItem(
         icon: Icon(
           key: ValueKey(tabName),
           isActivated ? activeIcon : inActiveIcon,
-          color:
-              isActivated ? context.appColors.iconButton : context.appColors.iconButtonInactivate,
+          color: isActivated
+              ? context.appColors.iconButton
+              : context.appColors.iconButtonInactivate,
         ),
         label: tabName);
   }
